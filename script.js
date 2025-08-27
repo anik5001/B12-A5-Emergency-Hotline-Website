f340bea630c34f058b5f582844ec8c0ca88e1ed7
anik5001 <aniksarker047@gmail.com>
2025-08-27 12:36:57 +0600

    calling button and clear button and mobile responsive done

--- script.js
+++ script.js
@@ -12,6 +12,53 @@ document
 
     // call button
     if (targetData.className.includes("call-btn")) {
-      console.log("clicked call button");
+      let coinValue = Number(document.getElementById("coin-number").innerText);
+
+      const title = targetData.parentNode.parentNode.children[1].innerText;
+      const hotNumber = targetData.parentNode.parentNode.children[3].innerText;
+
+      const serviceName =
+        targetData.parentNode.parentNode.children[2].innerText;
+
+      if (coinValue >= 20) {
+        alert(`📞 calling ${serviceName} ${hotNumber}...`);
+        coinValue = coinValue - 20;
+        document.getElementById("coin-number").innerText = coinValue;
+
+        const date = new Date().toLocaleTimeString();
+
+        // create a childnode
+        var cardNode = document.createElement("div");
+        const callHistoryParent = document.getElementById("card-section");
+        cardNode.innerHTML = `
+              <div class="flex justify-between items-center bg-gray-50 p-1 py-4 rounded-lg mt-3">
+          <div>
+            <h1 class="font-semibold">${title}</h1>
+            <p>${hotNumber}</p>
+          </div>
+          <p>${date}</p>
+        </div>
+            `;
+
+        callHistoryParent.appendChild(cardNode);
+      } else {
+        alert("❌ You don't have enough coins. You need at least 20 coins.");
+      }
     }
+
+    // copy button
+    if (targetData.className.includes("copy-btn")) {
+      let copyValueTotal = Number(
+        document.getElementById("copy-count").innerText
+      );
+      copyValueTotal = copyValueTotal + 1;
+      document.getElementById("copy-count").innerText = copyValueTotal;
+    }
+  });
+
+// call history button
+document
+  .getElementById("call-history-clear-btn")
+  .addEventListener("click", function () {
+    document.getElementById("card-section").innerHTML = " ";
   });
